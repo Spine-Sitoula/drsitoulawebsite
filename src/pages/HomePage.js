443e7771b64dfa638d1b0c37a34c67fe32befeff
Gaurav Kharel <officialgauravkharel@gmail.com>
2026-05-23 16:39:11 +0545

    Fix person photo path

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -56,7 +56,7 @@ const HomePage = () => {
           <div className="photo-card">
             <div className="person-photo">
               <img 
-                src="/prakash.jpg" 
+                src={'${process.env.PUBLIC_URL}/prakash.jpg'} 
                 alt="Dr. Prakash Sitoula" 
                 onError={(e) => e.target.src = 'https://placehold.co/400x500?text=Dr.+Prakash+Sitoula'}
               />
