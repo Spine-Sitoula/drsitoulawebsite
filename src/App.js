e4f4a722187d043973a4b91fb7b6a05d4a495e5c
Gaurav Kharel <officialgauravkharel@gmail.com>
2026-05-23 19:30:19 +0545

    Deploy correct build - remove basename paths

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/drsitoulawebsite">
+    <Router>
       <div className="app">
         <div className="bg-layer"></div>
         <div className="container">
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -56,7 +56,7 @@ const HomePage = () => {
           <div className="photo-card">
             <div className="person-photo">
               <img 
-                src="/drsitoulawebsite/drprakash.jpg"  
+                src="/drprakash.jpg"
                 alt="Dr. Prakash Sitoula"
                 onError={(e) => e.target.src = 'https://placehold.co/400x500?text=Dr.+Prakash+Sitoula'}
               />
